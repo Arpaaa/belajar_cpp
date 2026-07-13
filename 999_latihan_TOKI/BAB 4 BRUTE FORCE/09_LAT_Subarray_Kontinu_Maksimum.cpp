aacdf73aae493025b560c24c64f257d8044956ad
Arpaaa <ernawatiarfa898@gmail.com>
2026-07-13 08:11:17 +0700

    feat(competitive-programming): add exercise for brute force

---
+++ 999_latihan_TOKI/BAB 4 BRUTE FORCE/09_LAT_Subarray_Kontinu_Maksimum.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop for (int i=0;i<n;i++)
+int main() {
+    int n, k; cin >> n >> k;
+    int a[n];
+    loop { cin >> a[i]; }
+    int mlen=0;
+
+}
